31fc127b871df28c5ab91c2903563121e5b6816b
anything <else>
2022-08-05 05:31:46 +0000

    use prediction to speed up

--- beautiful-strings.cpp
+++ beautiful-strings.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int z[5001][5001], square_substring_count[5001][5001];
+int z[5001][5001], square_substring_accumulated_length[5001][5001], square_substring_count[5001][5001];
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
@@ -29,6 +29,22 @@ int main()
                     z[i][j] = 0;
             }
 
+        fill(square_substring_accumulated_length[0], square_substring_accumulated_length[0] + n + 1, 0);
+        for (int i = 1; i < n; i++)
+        {
+            int counter = 0;
+            fill(square_substring_accumulated_length[i], square_substring_accumulated_length[i] + n + 1, 0);
+            for (int length = 1; length <= min(i, n - i); length++)
+            {
+                if (z[i - length][i] >= length)
+                    counter += length;
+                square_substring_accumulated_length[i][length] = counter;
+            }
+
+            for (int length = min(i, n - i) + 1; length <= n; length++)
+                square_substring_accumulated_length[i][length] = counter;
+        }
+
         fill(square_substring_count[0], square_substring_count[0] + n + 1, 0);
         for (int i = 1; i < n; i++)
         {
@@ -49,41 +65,14 @@ int main()
         // int unofficial = 0;
 
         for (int i = 0; i < n; i++)
-            for (int j = i + 1; j < n; j++)
+            for (int j = i + 3; j < n; j++)
             {
                 int w = min(j - i, z[i][j]);
 
                 if (w == 0)
                     continue;
 
-                // if (square_substring_count[i][w - 1] != 0)
-                // {
-                //     for (int length_a = 1; length_a <= w - 1; length_a++)
-                //     {
-                //         int exists = square_substring_count[i][length_a] - square_substring_count[i][length_a - 1];
-
-                //         if (!exists)
-                //             continue;
-
-                //         // AABCAB
-
-                //         int length_b = w - length_a;
-
-                //         int left = i - length_a;
-                //         int right = j + w - 1;
-
-                //         int length = right - left + 1;
-
-                //         cout << "Recorded: left = " << left << ", right = " << right << ", length_a = " << length_a << ", length_b = " << length_b << "\n";
-                //         cout << "Middle interval: " << i << " " << j - 1 << "\n";
-                //         if (3 * length_a + 2 * length_b < length)
-                //         {
-                //             unofficial++;
-                //         }
-                //     }
-                // }
-
-                result += square_substring_count[i][w - 1];
+                result += square_substring_count[i][w - 1] * min(z[i][j], j - i - 1) - square_substring_accumulated_length[i][w - 1];
             }
 
         cout << result << "\n";
